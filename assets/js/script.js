3f3aa0667752f97be5c28ea956ac97c3fdc7e085
Ali Khalife <alikhalife05@gmail.com>
2022-03-23 16:22:18 +0100

    fetching data from table1 and creating a bar chart

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+let table1 = document.getElementById("table1");
+let table2 = document.getElementById("table2");
+console.log(table1)
+
+
+// creating chart for table1
+function BuildChart(labels, values, chartTitle) {
+  var ctx = document.getElementById("myChart").getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: labels, // Our labels
+      datasets: [{
+        label: chartTitle, // Name the series
+        data: values, // Our values
+        backgroundColor: [ // Specify custom colors
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [ // Add custom color borders
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1 // Specify bar border width
+      }]
+    },
+    options: {
+      responsive: true, // Instruct chart js to respond nicely.
+      maintainAspectRatio: false, // Add to prevent default behavior of full-width/height 
+    }
+  });
+  return myChart;
+}
+
+
+
+// getting data from table1
+
+
+var json = []; // First row needs to be headers 
+var headers =[];
+for (var i = 0; i < table1.rows[0].cells.length; i++) {
+  headers[i] = table1.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi, '');
+}
+
+// Go through cells 
+for (var i = 1; i < table1.rows.length; i++) {
+  var tableRow = table1.rows[i];
+  var rowData = {};
+  for (var j = 0; j < tableRow.cells.length; j++) {
+    rowData[headers[j]] = tableRow.cells[j].innerHTML;
+  }
+
+  json.push(rowData);
+}
+
+console.log(json);
+
+
+
+// Map JSON values back to label array
+var labels = json.map(function (e) {
+  return e.country;
+});
+console.log(labels); // ["2016", "2017", "2018", "2019"]
+
+// Map JSON values back to values array
+var values = json.map(function (e) {
+  return e["number(inthousands)"];
+});
+console.log(values);
+
+var chart = BuildChart(labels, values, " Offences recorded by the police, 2002-12");
+
+
+
