9d853736dc6e7633cb2761f765c0d03c8a592d02
SIRIEZ-Axel <axel.siriez@gmail.com>
2022-03-25 12:43:00 +0100

    day 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,11 @@
-// charts build 
+// chart 1 build 
 function BuildChart(labels, values, chartTitle) {
     var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012',], // Our labels
+            labels: ['2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012',], 
             datasets: [{
-                label: labels,
-                data: values,
-                fill: false,
-                borderColor: 'rgb(75, 192, 192)',
-                tension: 0.1
-            }]
-            [{
                 label: labels,
                 data: values,
                 fill: false,
@@ -29,7 +22,6 @@ function BuildChart(labels, values, chartTitle) {
 }
 
 // HTML To JSON Script 
-// *Forked* from https://johndyer.name/html-table-to-json/
 var table1 = document.getElementById('table1');
 var json = []; // first row needs to be headers 
 var headers = [];
@@ -62,6 +54,31 @@ console.log(values);
 var chart = BuildChart();
 
 
+// build chart 2
+var ctx2 = document.getElementById('chart2').getContext('2d')
+
+var chart2 = new chart(ctx2, {    
+    data : {
+        type : 'line',
+        labels: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+        options: {
+            responsive : true,
+        }
+})
 
 
 
@@ -70,38 +87,36 @@ var chart = BuildChart();
 
 
 // Live Updating Charts from JSON Data API
-
 window.onload = function() {
 	var dataPoints = [];
 	var chart;
+    var ctxLive = document.getElementById("chartContainer").getContext('2d')
 	$.getJSON("https://canvasjs.com/services/data/datapoints.php", function(data) {  
 		$.each(data, function(key, value){
 			dataPoints.push({x: value[0], y: parseInt(value[1])});
 		});
-		chart = new CanvasJS.Chart("chartContainer",{
+		chart = new Chart(ctxLive,{
 			title:{
-				text:"Datapoints.php"
+				text:"Live Chart with dataPoints from External JSON"
 			},
 			data: [{
-                type: "line",
+				type: "line",
 				dataPoints : dataPoints,
 			}]
 		});
 		chart.render();
 		updateChart();
 	});
-
-    function updateChart() {
-    $.getJSON("https://canvasjs.com/services/data/datapoints.php" + (dataPoints.length + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json", function(data) {
-        $.each(data, function(key, value) {
-            dataPoints.push({
-            x: parseInt(value[0]),
-            y: parseInt(value[1])
-            });
-        });
-        chart.render();
-        setTimeout(function(){updateChart()}, 1000);
-    });
-    }
-
+	function updateChart() {
+	$.getJSON("https://canvasjs.com/services/data/datapoints.php" + (dataPoints.length + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json", function(data) {
+		$.each(data, function(key, value) {
+			dataPoints.push({
+			x: parseInt(value[0]),
+			y: parseInt(value[1])
+			});
+		});
+		chart.render();
+		setTimeout(function(){updateChart()}, 1000);
+	});
+	}
 }
