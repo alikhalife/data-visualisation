b3f4d78be60e70909d41a16869412c5a5e261085
Ali Khalife <alikhalife05@gmail.com>
2022-03-25 16:51:47 +0100

    creating chart for table1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,13 @@
-let table1 = document.getElementById("table1");
-let table2 = document.getElementById("table2");
-console.log(table1)
-
-
 // creating chart for table1
-function BuildChart(labels, values, chartTitle) {
+function BuildChart(labels, datasets, chartTitle) {
+  
   var ctx = document.getElementById("myChart").getContext('2d');
   var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-      labels: labels, // Our labels
-      datasets: [{
-        label: chartTitle, // Name the series
-        data: values, // Our values
-        backgroundColor: [ // Specify custom colors
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [ // Add custom color borders
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1 // Specify bar border width
-      }]
+      labels: labels,
+      datasets: datasets
+      
     },
     options: {
       responsive: true, // Instruct chart js to respond nicely.
@@ -42,43 +19,108 @@ function BuildChart(labels, values, chartTitle) {
 
 
 
-// getting data from table1
+//selecting data from HTML document
+let table1 = document.getElementById("table1");
 
 
-var json = []; // First row needs to be headers 
-var headers =[];
-for (var i = 0; i < table1.rows[0].cells.length; i++) {
-  headers[i] = table1.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi, '');
+// getting each dataset from table1 and storing them in datasets
+var datasets1 = [];
+var label = [];
+var rawData = table1.rows
+var lines = [];
+
+
+//getting data from each row and storing them in datasets1 array
+for (let i = 2; i < rawData.length; i++) {
+  // console.log(datasetsRows.item(i)) //printing each row of my table
+  // console.log(table1.rows[i].cells) // printing each column of my table
+ 
+  var data1 = table1.rows.item(i);
+  var data1Arr = Array.from(data1.children)
+  
+  data1Arr.shift();
+
+  datasets1.push(data1Arr);
+  
 }
+console.log(datasets1)
 
-// Go through cells 
-for (var i = 1; i < table1.rows.length; i++) {
-  var tableRow = table1.rows[i];
-  var rowData = {};
-  for (var j = 0; j < tableRow.cells.length; j++) {
-    rowData[headers[j]] = tableRow.cells[j].innerHTML;
+datasets1.forEach(row => {
+  //generating random color
+const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+  var dataset = {
+    label: row.shift().innerText, 
+    data: [],
+    borderColor: "#" + randomColor
+};
+  row.forEach(cell => {
+    dataset.data.push(parseInt(cell.innerText))
+  })
+  console.log("raw finish")
+  lines.push(dataset)
+})
+
+
+//getting each year from table1 and storing them in labels
+var labels = []
+
+var years = table1.rows.item(1);
+var yearsArr = Array.from(years.children);
+yearsArr.shift()
+yearsArr.forEach(element => {
+  var year = element.innerText;
+  labels.push(year)
+})
+
+
+
+
+
+var datasets = [
+  {
+    label: "belgium", //here should be my second column
+    data: [1012.8,	1007.8,	1013.7,	999.4, 1022.8,	1034.4,	1043.6,	1067.3,	1072.0,	1111.0,	1073.8],
+    borderColor:"red",
   }
+]
 
-  json.push(rowData);
-}
-
-console.log(json);
-
+console.log(lines)
+var chart = BuildChart(labels, lines, " Offences recorded by the police, 2002-12");
 
 
-// Map JSON values back to label array
-var labels = json.map(function (e) {
-  return e.country;
-});
-console.log(labels); // ["2016", "2017", "2018", "2019"]
-
-// Map JSON values back to values array
-var values = json.map(function (e) {
-  return e["number(inthousands)"];
-});
-console.log(values);
-
-var chart = BuildChart(labels, values, " Offences recorded by the police, 2002-12");
 
 
+
+
+// Live Chart with API
+
+// window.addEventListener("load", function () {
+//   var dataPoints = [];
+//   var chart;
+
+//   fetch("https://canvasjs.com/services/data/datapoints.php", {cache: "no-store"})
+//       .then((response) => {
+//           return response.json()
+//       })
+//       .then((data) => {
+//           data.forEach(element => {
+//               dataPoints.push({ x: element[0], y: element[1] })
+//           });
+//           chart = new CanvasJS.Chart("remoteDataChart", {
+//               title: {
+//                   text: "Datapoints.php"
+//               },
+//               data: [{
+//                   type: "line",
+//                   dataPoints: dataPoints,
+//               }]
+//           });
+//           console.log(chart)
+//           chart.render();
+//           // updateChart();
+
+//       })
+
+//   })
 
