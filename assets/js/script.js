d08db2c851bf7a0ebaaef992e3db92915f8bd93c
SIRIEZ-Axel <axel.siriez@gmail.com>
2022-03-25 10:31:50 +0100

    add index & script live charts

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,43 +2,64 @@
 function BuildChart(labels, values, chartTitle) {
     var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
-            labels: labels, // Our labels
+            labels: ['2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012',], // Our labels
             datasets: [{
-                label: chartTitle, // Name the series
-                data:  values , // Our values
-                backgroundColor: [ // Specify custom colors
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [ // Add custom color borders
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1 // Specify bar border width
+                label: labels,
+                data: values,
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }]
+            [{
+                label: labels,
+                data: values,
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
             }]
         },
         options: {
             responsive: true, // Instruct chart js to respond nicely.
-            maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height 
+            legend: false
         }
     });
     return myChart;
 }
 
-//Get data from table
-var table = document.getElementById("table1")
+// HTML To JSON Script 
+// *Forked* from https://johndyer.name/html-table-to-json/
+var table1 = document.getElementById('table1');
+var json = []; // first row needs to be headers 
+var headers = [];
+for (var i = 0; i < table1.rows[0].cells.length; i++) {
+    headers[i] = table1.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi, '');
+}
 
+// go through cells 
+for (var i = 1; i < table1.rows.length; i++) {
+    var tableRow = table1.rows[i];
+    var rowData = {};
+    for (var j = 0; j < tableRow.cells.length; j++) {
+        rowData[headers[j]] = tableRow.cells[j].innerHTML;
+    }
+    json.push(rowData);
+}
+console.log(json);
 
+// Map json values back to label array
+var labels = json.map(function (e) {
+    return e.country;
+});
+console.log(labels);
+
+// Map json values back to values array
+var values = json.map(function (e) {
+    return e['number(inthousands)'];
+});
+console.log(values);
+var chart = BuildChart();
 
 
 
@@ -62,7 +83,7 @@ window.onload = function() {
 				text:"Datapoints.php"
 			},
 			data: [{
-				type: "line",
+                type: "line",
 				dataPoints : dataPoints,
 			}]
 		});
@@ -70,16 +91,17 @@ window.onload = function() {
 		updateChart();
 	});
 
-	function updateChart() {
-	$.getJSON("https://canvasjs.com/services/data/datapoints.php" + (dataPoints.length + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json", function(data) {
-		$.each(data, function(key, value) {
-			dataPoints.push({
-			x: parseInt(value[0]),
-			y: parseInt(value[1])
-			});
-		});
-		chart.render();
-		setTimeout(function(){updateChart()}, 1000);
-	});
-	}
+    function updateChart() {
+    $.getJSON("https://canvasjs.com/services/data/datapoints.php" + (dataPoints.length + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json", function(data) {
+        $.each(data, function(key, value) {
+            dataPoints.push({
+            x: parseInt(value[0]),
+            y: parseInt(value[1])
+            });
+        });
+        chart.render();
+        setTimeout(function(){updateChart()}, 1000);
+    });
+    }
+
 }
